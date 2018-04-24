7a2ca11aa8785727365d11cc2f533843f6de3f9b
jascci <joanna.chopard@yahoo.com>
2018-04-24 23:14:12 +0200

    changes to use minimax

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -4,25 +4,135 @@ var HMAN_SYMBOL;    // Human symbol
 var MACH_SYMBOL;    // Machine symbol
 var GAMEBOARD;      // Game board table
 var BOARD_LENGTH = 3;
+var FC;
+
+function getEmptySquares(board)  {
+  var emptySquares = [];
+  for(var i = 0; i < BOARD_LENGTH; i++ )  {
+    for(var j = 0; j < BOARD_LENGTH; j++ )  {
+      if ( board[i][j] == '' )  {
+        emptySquares.push({x:i, y:j});
+      }
+    }
+  }
+  return emptySquares;
+}
+
+function minimax(curBoard, curPlayer)  {
+  FC++;
+  var emptySquares = getEmptySquares(curBoard);
+
+  // Check for terminal states: win, loss, tie
+  if (isAWin(curBoard, MACH_SYMBOL))  {
+    return {score:10};
+  } else if (isAWin(curBoard, HMAN_SYMBOL))  {
+    return {score:-10};
+  } else if ( emptySquares.length == 0 )  {
+    return {score: 0};
+  }
+
+  // Try out all the empty squares and subsequent scenarios
+  var allMoves = [];
+  for(var i = 0; i < emptySquares.length; i++ )  {
+    // Set the empty square to the current player
+    curBoard[emptySquares[i].x][emptySquares[i].y] = curPlayer;
+    var move = {};
+    move.x = emptySquares[i].x;
+    move.y = emptySquares[i].y;
+    var result;
+    if ( curPlayer == MACH_SYMBOL )  {
+      result = minimax(curBoard, HMAN_SYMBOL );
+      
+    } else {
+      result = minimax(curBoard, MACH_SYMBOL );
+    }
+    move.score = result.score;
+
+    // Reset the square to undefined
+    curBoard[emptySquares[i].x][emptySquares[i].y] = '';
+    allMoves.push(move);
+  }
+
+  // Select the best move according to the curPlayer:
+  // if curPlayer is MACH, get the move with the highest score
+  // if curPlayer is HMAN, get the move with the lowest score 
+  var bestMoveIndex;
+  if (curPlayer == MACH_SYMBOL)  {
+    bestScore = -1000;
+    for(var k = 0; k < allMoves.length; k++ ) {
+      if ( allMoves[k].score > bestScore )  {
+        bestMoveIndex = k;
+        bestScore = allMoves[k].score;
+      }
+    }
+  
+    /*bestMove = allMoves.reduce(function(acc, curMove)  {
+      if (curMove.score > acc.score)  {
+        return curMove;
+      } else {
+        return acc;
+      }
+    }, {score:-1000});*/
+  } else {
+    bestScore = 1000;
+    for(var k = 0; k < allMoves.length; k++ ) {
+      if ( allMoves[k].score < bestScore )  {
+        bestMoveIndex = k;
+        bestScore = allMoves[k].score;
+      }
+    }
+
+     /*bestMove = allMoves.reduce(function(acc, curMove)  {
+      if (curMove.score < acc.score)  {
+        return curMove;
+      } else {
+        return acc;
+      }
+    }, {score:1000});*/
+  }
+
+  return allMoves[bestMoveIndex];
+}
+
+function copy(origBoard)  {
+  var copy =  new Array(origBoard.length);
+  for(var i = 0; i < origBoard.length; i++)  {
+    copy[i] = new Array(origBoard[i].length);
+    for(var j = 0; j < origBoard[i].length; j++)  {
+      copy[i][j] = origBoard[i][j];
+    }
+  }
+  return copy;
+}
 
 function machPlay() {
+  FC = 0;
   console.log("machine playing");
+  var boardCopy = copy(GAMEBOARD);
+
+  var bestMove = minimax(boardCopy, MACH_SYMBOL);
+  var randX = bestMove.x;
+  var randY = bestMove.y;
+  console.log(`bestMove = ${bestMove.x} , ${bestMove.y}, ${bestMove.score}`);
+  console.log(`function calls = ${FC}`);
+
+  /*
   var randX = Math.floor((Math.random() * 3));
   var randY = Math.floor((Math.random() * 3));
   while(typeof GAMEBOARD[randX][randY] !== 'undefined') {
     randX = Math.floor((Math.random() * 3));
     randY = Math.floor((Math.random() * 3));
-  }
+  }*/
   
   GAMEBOARD[randX][randY] = MACH_SYMBOL;
-  //$(".cell").find(`[data-position='{"x":${randX}, "y":${randY}}']`).html(MACH_SYMBOL);
-  //var cell = $(".cell").find(`[data-x='0']`).html(MACH_SYMBOL); //[data-y='${randY}']`); //.html(MACH_SYMBOL);
   var cell = $(`button[data-x='${randX}'][data-y='${randY}']`).html(MACH_SYMBOL);
-  //var cell = $('button[data-position={"x":0, "y":0}]').html(MACH_SYMBOL);
   if (cell.length > 0)  {
     console.log(`cell = ${cell}`);
   }
   console.log(`randX, randY = ${randX}, ${randY}`);
+  if(isAWin(GAMEBOARD, MACH_SYMBOL))  {
+    console.log('MACH wins!');
+  }
   IS_MACH_TURN = false;
 }
 
@@ -49,44 +159,40 @@ function init() {
   GAMEBOARD = new Array(BOARD_LENGTH);
   for (var i = 0; i < GAMEBOARD.length; i++) {
     GAMEBOARD[i] = new Array(BOARD_LENGTH);
+    for (var j = 0; j < BOARD_LENGTH; j++)  {
+      GAMEBOARD[i][j] = '';
+    }
   }  
 }
 
-function isAWin() {
+function isAWin(board, symbol) {
   // horizontal
   for (var j = 0; j < BOARD_LENGTH; j++)  {
-    if ( typeof GAMEBOARD[0][j] !== 'undefined' && 
-         GAMEBOARD[0][j] == GAMEBOARD[1][j] &&
-         GAMEBOARD[1][j] == GAMEBOARD[2][j] )  {
+    if ( board[0][j] == symbol && board[1][j] == symbol && board[2][j] == symbol )  {
       return true;
     }
   }
   
   // vertical
   for (var i = 0; i < BOARD_LENGTH; i++)  {
-    if ( typeof GAMEBOARD[i][0] !== 'undefined' && 
-         GAMEBOARD[i][0] == GAMEBOARD[i][1] &&
-         GAMEBOARD[i][1] == GAMEBOARD[i][2] )  {
+    if ( board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol )  {
       return true;
     }
   }
   
   // diagonal upper-left to down-right
-  if ( typeof GAMEBOARD[0][0] !== 'undefined' && 
-       GAMEBOARD[0][0] == GAMEBOARD[1][1] &&
-       GAMEBOARD[1][1] == GAMEBOARD[2][2] ) {
+  if ( board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) {
     return true;
   }
   
   // diagonal upper-right to down-left
-  if ( typeof GAMEBOARD[2][0] !== 'undefined' && 
-       GAMEBOARD[2][0] == GAMEBOARD[1][1] &&
-       GAMEBOARD[1][1] == GAMEBOARD[0][2] ) {
+  if ( board[2][0] == symbol && board[1][1] == symbol && board[0][2] == symbol) {
     return true;
   }
   
   return false;
 }
+
 
 $(document).ready(function() {
   init();
@@ -96,13 +202,12 @@ $(document).ready(function() {
       return;
     }
     
-    //var pos = $(this).data('position');
     var x = $(this).data('x');
     var y = $(this).data('y');
     console.log(`pos.x = ${x}`);
     console.log(`pos.y = ${y}`);
     
-    if ( typeof GAMEBOARD[x][y] !== 'undefined' ) { 
+    if ( GAMEBOARD[x][y] != '' ) { 
       console.log("This cell is already occupied.");
       return; 
     }
@@ -116,9 +221,6 @@ $(document).ready(function() {
       }
     }*/
     
-    if (isAWin())  {
-      console.log("You WIN!");
-    }
     
     IS_MACH_TURN = true;
   });
