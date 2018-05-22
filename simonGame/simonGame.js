826139bb9f030ff24e0f71314153bfadd3caae42
jascci <joanna.chopard@yahoo.com>
2018-05-22 16:41:28 +0200

    first attempt at svg graphics (cup filled with water)

--- simonGame/simonGame.js
+++ simonGame/simonGame.js
@@ -1,5 +1,6 @@
 var NUM_CUR_STEPS;
 var SESSION_ID;
+var TIMER_ID;
 var IS_SIMON_TURN;
 var IS_HUMAN_TURN;
 var SIMON_SERIES;
@@ -7,11 +8,20 @@ var HUMAN_SERIES;
 var PLAYLIST;
 var TUNE_MIN = 0;
 var TUNE_MAX = 3;
-var AUDIOS = [
+/*var AUDIOS = [
   "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3",
   "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3",
   "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3",
   "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"
+];*/
+var AUDIOS = [
+  "https://cui.unige.ch/~chopard/joanna/simonGame/sound/xylophone-c.mp3",
+  "https://cui.unige.ch/~chopard/joanna/simonGame/sound/xylophone-d.mp3",
+  "https://cui.unige.ch/~chopard/joanna/simonGame/sound/xylophone-e.mp3",
+  "https://cui.unige.ch/~chopard/joanna/simonGame/sound/xylophone-f.mp3",
+  "https://cui.unige.ch/~chopard/joanna/simonGame/sound/xylophone-a.mp3",
+  "https://cui.unige.ch/~chopard/joanna/simonGame/sound/xylophone-b.mp3",
+  "https://cui.unige.ch/~chopard/joanna/simonGame/sound/xylophone-c2.mp3",
 ];
 
 function playNext() {
@@ -24,26 +34,31 @@ function playNext() {
   }
 }
 
+function replay() {
+  clearArray(PLAYLIST);
+  PLAYLIST = SIMON_SERIES.slice();
+  console.log(`replaying PLAYLIST = ${PLAYLIST}`);
+  playNext();
+}
+
 function checkInput() {
   // Beep if user input is too slow
   if ( arraysEqual(SIMON_SERIES, HUMAN_SERIES) ) {
     console.log("you got it right");
   } else {
-    console.log("wrong. try again");
+    console.log("sorry, time out.");
   }
-  IS_SIMON_TURN = true;
-  
+  clearTimeout(TIMER_ID);
 }
 
 function startGame() {
   console.log("playing game");
   if (IS_SIMON_TURN)  {
-    // create random series
-    for (var i=0; i < NUM_CUR_STEPS; i++)  {
-      var rand = getRandomTune();
-      console.log(`rand = ${rand}`);
-      SIMON_SERIES.push(rand);
-    }
+    // create random tune and append to the series
+    var rand = getRandomTune();
+    console.log(`rand = ${rand}`);
+    SIMON_SERIES.push(rand);
+   
     PLAYLIST = SIMON_SERIES.slice();
     console.log(`PLAYLIST = ${PLAYLIST}`);
     // play tune series
@@ -52,7 +67,7 @@ function startGame() {
 
     IS_SIMON_TURN = false;
     // wait for human inputs, with a timeout
-    setTimeout(checkInput, 10000);
+    TIMER_ID = setTimeout(checkInput, 15000);
   } else {
 
 
@@ -76,8 +91,14 @@ function arraysEqual(a, b) {
   return true;
 }
 
+function clearArray(array) {
+  while (array.length) {
+    array.pop();
+  }
+}
+
 function init() {
-  NUM_CUR_STEPS = 5;
+  NUM_CUR_STEPS = 1;
   IS_SIMON_TURN = true;
   SIMON_SERIES = [];
   PLAYLIST = [];
@@ -91,16 +112,36 @@ $(document).ready(function() {
     var pos = $(this).data('position');
     HUMAN_SERIES.push(pos);
     
-    /*
+
+
     if (HUMAN_SERIES.length == SIMON_SERIES.length)  {
+      clearTimeout(TIMER_ID);
       // check series
       if ( arraysEqual(SIMON_SERIES, HUMAN_SERIES) ) {
         console.log("you got it right");
+        NUM_CUR_STEPS++;
+        IS_SIMON_TURN = true;
+        clearArray(HUMAN_SERIES);
+
       } else {
         console.log("wrong. try again");
+        replay();
+        clearArray(HUMAN_SERIES);
       }
-      IS_SIMON_TURN = true;
-    }*/
+    } else {
+      // check for correctness up to the current input length
+      for (var i = 0; i < HUMAN_SERIES.length; i++)  {
+        if (HUMAN_SERIES[i] != SIMON_SERIES[i])  {
+          console.log("wrong.  try again");
+          replay();
+          clearArray(HUMAN_SERIES);
+        } 
+      }
+
+    }
+
+
+
   });
 
   $("#startGame").click(function(event)  {
@@ -111,6 +152,29 @@ $(document).ready(function() {
     clearInterval(SESSION_ID);
     console.log("Game cancelled.");
   });
+
+  $("#test").click(function(event)  {
+    $("#ripple_a1")
+      //.velocity({ opacity: 1.0 }, "EaseInOutElastic")
+      .velocity("transition.expandIn", { stagger: 300 })
+      .velocity({ opacity: 0.0 });
+    $("#ripple_b1")
+      //.velocity({ opacity: 1.0 }, "EaseInOutElastic")
+      .velocity("transition.expandIn", { stagger: 300 })
+      .velocity({ opacity: 0.0 });
+
+    $("#ripple_c1")
+      //.velocity({ opacity: 1.0 }, "EaseInOutElastic")
+      .velocity("transition.expandIn", { stagger: 300 })
+      .velocity({ opacity: 0.0 });
+
+    $("#ripple_d1")
+      //.velocity({ opacity: 1.0 }, "EaseInOutElastic")
+      .velocity("transition.expandIn", { stagger: 300 })
+      .velocity({ opacity: 0.0 });
+
+  });
+
 
   //$('#audioPlayer')[0].addEventListener("ended", function() {
   //  console.log("ended event listener");});
